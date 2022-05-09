7b417326258e9a45f1fd36342f7d152791d2c197
Atul Prajapati <atulkprajapati2000@gmail.com>
2022-05-09 20:10:49 +0530

    our education website is ready to use

---
+++ about.css
@@ -0,0 +1,168 @@
+#about-home
+{
+	background-image: linear-gradient(rgba(9, 5, 54, 0.3), rgba(5,4,46,0.7)), url("img/back1.jpg");
+	width: 100%;
+	height: 60vh;
+	background-size: cover;
+	background-position: center center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	padding-top: 40px;
+}
+
+#about-home h2
+{
+	color: #fff;
+	font-size: 2.2rem;
+	letter-spacing: 1px;
+}
+
+#about-container
+{
+	display: flex;
+	align-items: center;
+	padding: 8vw 8vw 2vw 8vw;
+}
+
+#about-container .about-img
+{
+	width: 60%;
+	padding-right: 60px;
+}
+
+#about-container .about-img img
+{
+	width: 100%;
+}
+
+#about-container .about-text
+{
+	width: 40%;
+}
+
+#about-container .about-text h2
+{
+	color: #29303B;
+	padding-bottom: 15px;
+}
+
+#about-container .about-text p
+{
+	color: #686F7A;
+	font-weight: 300;
+}
+
+#about-container .about-text .about-fe
+{
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
+	margin-top: 30px;
+}
+
+#about-container .about-text .about-fe img
+{
+	width: 50px;
+	background-size: cover;
+	background-position: center;
+	margin-right: 20px;
+}
+
+#about-container .about-text .about-fe .fe-text
+{
+	width: 90%;
+}
+
+#about-container .about-text .about-fe .fe-text h5
+{
+	font-size: 16px;
+	color: #29303B;
+}
+
+
+
+/*------------------- Trust section starts from here ---------------*/
+#trust
+{
+	text-align: center;
+	padding: 8vw;
+}
+
+#trust .trust-img
+{
+	margin-top: 60px;
+	display: flex;
+	justify-content: space-between;
+}
+
+#trust .trust-img img
+{
+	width: 90px;
+	height: auto;
+}
+/*------------------- Trust section ends from here ---------------*/
+
+
+
+
+/*------------------------------------------------------------------*/
+/*------------------- Tablet screen starts from here ---------------*/
+/*------------------------------------------------------------------*/
+@media screen and (max-width: 768px)
+{
+	#about-container
+	{
+		padding: 8vw 4vw 2vw 4vw;
+	}
+
+	#about-container .about-img
+	{
+		padding-right: 30px;
+	}
+
+	#trust .trust-img img
+	{
+		width: 50px;
+		height: auto;
+	}
+}
+
+
+/*------------------------------------------------------------------*/
+/*------------------- Smartphone screen starts from here ---------------*/
+/*------------------------------------------------------------------*/
+@media screen and (max-width: 475px)
+{
+	#about-container
+	{
+		flex-direction: column-reverse;
+	}
+
+	#about-container .about-img
+	{
+		width: 100%;
+		padding: 0;
+	}
+
+	#about-container .about-text
+	{
+		width: 100%;
+		padding-bottom: 20px;
+	}
+
+	#trust .trust-img
+	{
+		margin-top: 40px;
+		flex-wrap: wrap;
+	}
+
+	#trust .trust-img img
+	{
+		width: 60px;
+		margin: 10px 15px;
+		height: auto;
+	}
+}
